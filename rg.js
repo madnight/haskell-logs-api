d0530f5058c658a5a3216f1d02ad6e6d67124b66
Fabian Beuke <mail@beuke.org>
2021-01-03 19:14:52 +0100

    prettier rg.js

--- rg.js
+++ rg.js
@@ -1,73 +1,77 @@
-'use strict';
+"use strict";
 
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
 function RipgrepError(error, stderr) {
-  this.message = error;
-  this.stderr = stderr;
+    this.message = error;
+    this.stderr = stderr;
 }
 
 function formatResults(stdout) {
-  stdout = stdout.trim();
+    stdout = stdout.trim();
 
-  if (!stdout) {
-    return [];
-  }
-  stdout = stdout.split("\n")
-  stdout.sort()
-  return stdout.map((line) => new Match(line));
+    if (!stdout) {
+        return [];
+    }
+    stdout = stdout.split("\n");
+    stdout.sort();
+    return stdout.map((line) => new Match(line));
 }
 
 module.exports = function ripGrep(cwd, options, searchTerm) {
-  if (arguments.length === 2 && typeof options === 'string') {
-    searchTerm = options;
-    options = {};
-  }
+    if (arguments.length === 2 && typeof options === "string") {
+        searchTerm = options;
+        options = {};
+    }
 
-  if (!cwd) {
-    return Promise.reject('No `cwd` provided');
-  }
+    if (!cwd) {
+        return Promise.reject("No `cwd` provided");
+    }
 
-  if (arguments.length === 1) {
-    return Promise.reject('No search term provided');
-  }
+    if (arguments.length === 1) {
+        return Promise.reject("No search term provided");
+    }
 
-  options.regex = options.regex || '';
-  options.globs = options.globs || [];
-  options.string = searchTerm || options.string || '';
+    options.regex = options.regex || "";
+    options.globs = options.globs || [];
+    options.string = searchTerm || options.string || "";
 
-  let execString = 'rg --column --max-count 1 --line-number --color never';
-  if (options.regex) {
-    execString = `${execString} -e ${options.regex}`;
-  } else if (options.string) {
-    execString = `${execString} -F ${options.string}`;
-  }
+    let execString = "rg --column --max-count 1 --line-number --color never";
+    if (options.regex) {
+        execString = `${execString} -e ${options.regex}`;
+    } else if (options.string) {
+        execString = `${execString} -F ${options.string}`;
+    }
 
-  execString = options.globs.reduce((command, glob) => {
-    return `${command} -g '${glob}'`;
-  }, execString);
+    execString = options.globs.reduce((command, glob) => {
+        return `${command} -g '${glob}'`;
+    }, execString);
 
-  return new Promise(function(resolve, reject) {
-    const oneGigaByte = 1024 * 1024 * 1024;
-    exec(execString, { cwd, maxBuffer: oneGigaByte }, (error, stdout, stderr) => {
-      if (!error || (error && stderr === '')) {
-        resolve(formatResults(stdout));
-      } else {
-        reject(new RipgrepError(error, stderr));
-      }
+    return new Promise(function (resolve, reject) {
+        const oneGigaByte = 1024 * 1024 * 1024;
+        exec(
+            execString,
+            { cwd, maxBuffer: oneGigaByte },
+            (error, stdout, stderr) => {
+                if (!error || (error && stderr === "")) {
+                    resolve(formatResults(stdout));
+                } else {
+                    reject(new RipgrepError(error, stderr));
+                }
+            }
+        );
     });
-  });
 };
 
 class Match {
-  constructor(matchText) {
-    matchText = matchText.split(':');
+    constructor(matchText) {
+        matchText = matchText.split(":");
 
-    this.file = matchText.shift();
-    this.line = parseInt(matchText.shift());
-    this.column = parseInt(matchText.shift());
-    this.match = matchText.join(':');
-  }
+        this.file = matchText.shift();
+        this.line = parseInt(matchText.shift());
+        this.column = parseInt(matchText.shift());
+        this.match = matchText.join(":");
+    }
 }
 
 module.exports.Match = Match;
